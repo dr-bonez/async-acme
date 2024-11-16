3fac01cb7cff67ffdef56a90a3f1490a1b5d1905
Aiden McClelland <me@drbonez.dev>
2024-11-15 17:37:41 -0700

    remove useless logging

--- src/jose.rs
+++ src/jose.rs
@@ -47,7 +47,6 @@ pub async fn jose_req(
         } else {
             log::error!("{}: HTTP {}", url, response.status_code());
         }
-        dbg!(response.text().await?);
         return Err(AcmeError::HttpStatus(response.status_code()));
     }
     Ok(response)
